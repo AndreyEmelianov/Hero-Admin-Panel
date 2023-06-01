cc630e2da27659ee3ad4c2968709b7371fe71f94
Andrey Emelianov <621077@mail.ru>
2023-06-01 12:43:12 +0300

    реализовал фильтрацию героев и их отображения исходя из выбранного фильтра

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -19,6 +19,7 @@ const HeroesAddForm = () => {
 	const [heroDescr, setHeroDescr] = useState('');
 	const [heroElement, setHeroElement] = useState('');
 
+	const { filters, filtersLoadingStatus } = useSelector((state) => state);
 	const dispatch = useDispatch();
 	const { request } = useHttp();
 
@@ -38,6 +39,26 @@ const HeroesAddForm = () => {
 		setHeroName('');
 		setHeroDescr('');
 		setHeroElement('');
+	};
+
+	const renderFilters = (filters, status) => {
+		if (status === 'loading') {
+			return <option>Загрузка элементов</option>;
+		} else if (status === 'error') {
+			return <option>Ошибка загрузки</option>;
+		}
+
+		if (filters && filters.length > 0) {
+			return filters.map(({ name, label }) => {
+				if (name === 'all') return;
+
+				return (
+					<option key={name} value={name}>
+						{label}
+					</option>
+				);
+			});
+		}
 	};
 
 	return (
@@ -87,10 +108,7 @@ const HeroesAddForm = () => {
 					onChange={(e) => setHeroElement(e.target.value)}
 				>
 					<option>Я владею элементом...</option>
-					<option value="fire">Огонь</option>
-					<option value="water">Вода</option>
-					<option value="wind">Ветер</option>
-					<option value="earth">Земля</option>
+					{renderFilters(filters, filtersLoadingStatus)}
 				</select>
 			</div>
 
