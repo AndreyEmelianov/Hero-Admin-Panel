467aae4ceb7510a167e90ba108f8d597c4efa0f2
Andrey Emelianov <621077@mail.ru>
2023-06-01 18:27:24 +0300

    использовал createAsyncThunk для создания запросов и extraReducers

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,10 +1,8 @@
-import { useHttp } from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { fetchFilters } from '../../actions';
-import { activeFilterChanged } from './filtersSlice';
+import { activeFilterChanged, fetchFilters } from './filtersSlice';
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
@@ -12,10 +10,9 @@ const HeroesFilters = () => {
 		(state) => state.filters
 	);
 	const dispatch = useDispatch();
-	const { request } = useHttp();
 
 	useEffect(() => {
-		dispatch(fetchFilters(request));
+		dispatch(fetchFilters());
 	}, []);
 
 	if (filtersLoadingStatus === 'loading') {
